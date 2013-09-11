32997e2351baa8db6dc31c2bcfe657b95a75533b
Ana Nelson <ana@ananelson.com>
2013-09-11 15:18:25 -0700

    Add skeleton project.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from grasshook.version import GRASSHOOK_VERSION
+
+setup(
+        author='Ana Nelson',
+        author_email='ana@ananelson.com',
+        description='Periodic Data Collection',
+        entry_points = {
+            'console_scripts' : [
+                'grasshook = grasshook.commands:run'
+                ]
+            },
+        include_package_data = True,
+        install_requires = [
+            ],
+        name='grasshook',
+        packages=find_packages(),
+        url='http://dexy.it/grasshook',
+        version=GRASSHOOK_VERSION
+        )
